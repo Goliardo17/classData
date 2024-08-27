65f90d997a056b7c4855ac8585706e322ee33c12
Goliardo17 <139957986+Goliardo17@users.noreply.github.com>
2024-08-28 00:02:45 +0300

    Update index.js

--- services/user/auth/index.js
+++ services/user/auth/index.js
@@ -3,9 +3,16 @@ const add = require("./add");
 const edit = require("./edit");
 
 const userAuthServices = {
-  get,
-  edit,
-  add,
+  do: {
+    add: add.run,
+    get: get.run,
+    edit: edit.run
+  },
+  test: {
+    add: add.test,
+    get: get.test,
+    edit: edit.test
+  }
 };
 
 module.exports = userAuthServices;
